ec6d85134c578e6457c2b0d289434cf0fff23b98
Akros LLC <zsmith@akros.llc>
2022-04-10 22:06:07 -0500

    linear search implemented and tested

--- linkedlist.py
+++ linkedlist.py
@@ -16,7 +16,7 @@ class LinkedList:
 
         curr_node = self.head
         while curr_node:
-            output_string += str(curr_node)
+            output_string += str(curr_node) + " "
             curr_node = curr_node.next
         
         return output_string
@@ -29,8 +29,40 @@ class LinkedList:
             self.tail = new_node
         else:
             new_node.prev = self.tail
+            new_node.prev.next = new_node
             self.tail = new_node
 
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            new_node.next.prev = new_node
+            self.head = new_node
+
+    def linear_search(self, data):
+        curr_a = self.head
+        curr_b = self.tail
+
+        while (curr_a.data != data and curr_b.data != data) and (curr_a != curr_b):
+            curr_a = curr_a.next
+            curr_b = curr_b.prev
+
+        if curr_a.data == data:
+            return curr_a
+        elif curr_b.data == data:
+            return curr_b
+        elif (curr_a == curr_b) and (curr_a.data != data and curr_b.data != data):
+            return None
+
+    # def remove(self, item):
+    #     if type(item) is Node:
+
+
 if __name__ == '__main__':
-    test_list = LinkedList([1, 2, 3, 4])
+    test_list = LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 'a'])
+    # pdb.set_trace()
     print(test_list)

d773c8952f34969ccb35e47674d80143e7348a38
Akros LLC <zsmith@akros.llc>
2022-04-10 21:06:28 -0500

    Added string representation.  Working on iterable insert on list creation

--- linkedlist.py
+++ linkedlist.py
@@ -1,3 +1,31 @@
-class linkedlist:
-    def __init__(self):
+from node import Node
+
+class LinkedList:
+    def __init__(self, data=None):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+        for item in data:
+            self.insert(item)
+
+    def __repr__(self):
+        output_string = ""
+
+        if self.head is None:
+            return None
+
+        curr_node = self.head
+        while curr_node:
+            output_string += curr_node
+            curr_node = curr_node.next
+        
+        return output_string
+
+    def insert(self, data):
+        new_node = Node(data)
+        
+        if self.tail is None:
+            
+        
         

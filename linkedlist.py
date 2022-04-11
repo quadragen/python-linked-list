d5f3482f994063f1981d6b9a2fde5edc1c072991
Akros LLC <zsmith@akros.llc>
2022-04-10 21:19:23 -0500

    testing node insertion

--- linkedlist.py
+++ linkedlist.py
@@ -1,4 +1,5 @@
 from node import Node
+import pdb
 
 class LinkedList:
     def __init__(self, data=None):
@@ -6,26 +7,30 @@ class LinkedList:
         self.tail = None
         self.length = 0
 
-        for item in data:
-            self.insert(item)
+        if data:
+            for item in data:
+                self.insert(item)
 
     def __repr__(self):
         output_string = ""
 
-        if self.head is None:
-            return None
-
         curr_node = self.head
         while curr_node:
-            output_string += curr_node
+            output_string += str(curr_node)
             curr_node = curr_node.next
         
         return output_string
 
     def insert(self, data):
         new_node = Node(data)
-        
+
         if self.tail is None:
-            
-        
-        
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail = new_node
+
+if __name__ == '__main__':
+    test_list = LinkedList([1, 2, 3, 4])
+    print(test_list)

85b93c81e73ef626acfa34fa8478de611894a41d
Zach Smith <zsmith@akros.llc>
2022-04-11 19:56:37 -0500

    testing2

---
+++ main.py
@@ -0,0 +1,8 @@
+from linkedlist import LinkedList
+
+test_list = LinkedList([45, 53, 2, 349, 43, 390, 209, 390234, 1823, 38, 41])
+print(test_list)
+print(test_list.tree)
+test_list.remove(2)
+print(test_list)
+print(test_list.tree)

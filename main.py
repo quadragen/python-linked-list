e1298753721ecf1b0522d8b90e2dcf42237401de
Zach Smith <zsmith@akros.llc>
2022-04-14 04:07:48 -0500

    found bug with first pop not removing from tree

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pdb
 
 test_list = LinkedList([45, 53, 2, 349, 43, 390, 209, 390234, 1823, 38, 41])
 string_list = LinkedList(["alpha", "beta", "gamma", "delta", "epsilon", "theta"])
-# pdb.set_trace()
+pdb.set_trace()
 print(test_list)
 print(string_list)
 print(test_list.sorted())
